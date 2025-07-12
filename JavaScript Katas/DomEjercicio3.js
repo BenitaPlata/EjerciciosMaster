d0d2a184b9520d3d4d1f7a02ff4d24fab9ae2ef6
BenitaPlata <itaplata.n@gmail.com>
2025-07-12 11:18:11 +0200

    Dom Ejercicio 3

--- JavaScript Katas/DomEjercicio3.js
+++ JavaScript Katas/DomEjercicio3.js
@@ -52,9 +52,65 @@ const countriesList = [
   { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=4>" },
   { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=5>" },
 ];
+const divCountries = document.createElement("div"); //creo un div
+document.body.appendChild(divCountries); //lo agrego al body
+
+for (const country of countriesList) {
+  // bucle for of para recorrer el array
+  const divCountry = document.createElement("div"); //para cada objeto del array creo un div
+  const title = document.createElement("h4"); // creo un h4
+  title.textContent = country.title; // le pongo el texto del titulo
+  divCountry.appendChild(title); // lo agrego al div
+  const img = document.createElement("img"); // creo un img
+  img.src = country.imgUrl; // le pongo la url del a imagen
+  divCountry.appendChild(img); // lo agrego al div
+
+  divCountries.appendChild(divCountry); // lo agrego al div principal
+}
 
 //3.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último
 //elemento de la serie de divs.
 
+const button = document.createElement("button"); // creo un boton
+button.textContent = "Eliminar último elemento"; // le pongo texto
+document.body.appendChild(button); // lo agrego al body porque si lo agrego al div podría eliminarlo :)
+button.addEventListener("click", function () {
+  // agrego un evento click al boton
+  divCountries.lastElementChild.remove(); // elimino el último elemento
+});
+
 //3.6 Basandote en el ejercicio anterior. Crea un botón para cada uno de los
 //divs que elimine ese mismo elemento del html.
+const countriesList2 = [
+  { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=1>" },
+  { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=2>" },
+  { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=3>" },
+  { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=4>" },
+  { title: "Random title", imgUrl: "<https://picsum.photos/300/200?random=5>" },
+];
+
+//REUTILIZO EL CÓDIGO ANTERIOR Y MODIFICO LO QUE ME PIDEN
+const divCountries2 = document.createElement("div"); //creo un div
+document.body.appendChild(divCountries2); //lo agrego al body
+
+for (const country of countriesList) {
+  // bucle for of para recorrer el array (en cada vuelta country es un objeto con title y imgUrl)
+  const divCountry = document.createElement("div"); //para cada objeto del array creo un divcountry que va a contener el titulo, la imagen y un botón
+  const title = document.createElement("h4"); // creo un h4
+  title.textContent = country.title; // le pongo el texto del titulo
+  divCountry.appendChild(title); // lo agrego al div
+  const img = document.createElement("img"); // creo un img
+  img.src = country.imgUrl; // le pongo la url del a imagen
+  divCountry.appendChild(img); // lo agrego al div
+
+  //CREO UN BOTON PARA CADA DIV
+  const button = document.createElement("button"); //creo el botón
+  button.textContent = "Eliminar"; // le pongo texto
+  divCountry.appendChild(button);
+  button.addEventListener("click", function () {
+    //añado un envento al botón que escucha el click
+    this.parentNode.remove(); // this se refiere al botón que fue pulsado. parentnode es el nodo padre del botón que es el < class= "divCountry">  AL LLAMAR ESTO LE DIGO: ELIMINA EL DOM DEL PADRE DE ESTE BOTÓN, OSEA EL DIV QUE CONTIENEN EL BOTÓN Y TAMBIÉN EL TÍTULO Y LA IMAGEN
+  });
+
+  divCountries2.appendChild(divCountry); // lo agrego al div principal ahora, ya que el divCountry está completamente formado con todos sus hijos
+}
