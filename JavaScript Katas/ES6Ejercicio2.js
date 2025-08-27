6e9dc867b226d095aaded88c23c7ee3d7fb5e6ee
BenitaPlata <itaplata.n@gmail.com>
2025-08-27 11:58:22 +0200

    cambio nombres

--- JavaScript Katas/ES6Ejercicio2.js
+++ JavaScript Katas/ES6Ejercicio2.js
@@ -14,18 +14,18 @@ const copiaToy = {...toy};
 
 /*2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
 spread operatos.*/
-const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsList1 = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
 
-const newArray = [...pointsList, ...pointsLis2];
+const newArray = [...pointsList1, ...pointsLis2];
 
 
 /*2.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
 con spread operators.*/
-const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyN = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
-const newObject = {...toy, ...toyUpdate};
+const newObject = {...toyN, ...toyUpdate};
 
 /*2.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
 pero sin editar el array inicial. De nuevo, usando spread operatos.*/
