094fedb461907672dc38627bf6a32907bc551a76
BenitaPlata <itaplata.n@gmail.com>
2025-09-03 17:25:34 +0200

    Create Ejercicio21.py

---
+++ Proyecto3-Katas-Python/Katas/Ejercicio21.py
@@ -0,0 +1,9 @@
+'''Crea una función que calcule el cubo de un número dado mediante una función lambda.'''
+
+# Paso 1: Definir la función lambda para calcular el cubo
+cubo = lambda x: x ** 3
+
+# Paso 2: Probar la función cubo con algunos números
+print(cubo(2)) 
+print(cubo(3))  
+print(cubo(5))  
